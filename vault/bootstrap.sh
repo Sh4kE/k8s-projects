8258c74dd855ce361d3f26fe150cf6b8edba9720
Michael Wittig <michael.wittig@noris.de>
2022-06-24 09:45:58 +0200

    update bootstrapping with latest stuff

--- vault/bootstrap.sh
+++ vault/bootstrap.sh
@@ -24,41 +24,40 @@ kubectl exec -ti vault-2 -- vault operator unseal $UNSEAL_TOKEN_2
 
 
 kubectl exec --stdin=true --tty=true vault-0 -- /bin/sh
+###############
 
-    vault secrets enable -path=kv kv-v2
-    vault kv put kv/k3s/argocd/config username="sh4ke" password="go sh4key go"
+vault login -address=https://vault.k3s.sh4ke.rocks $ROOT_TOKEN
 
-    vault auth enable kubernetes
-    vault write auth/kubernetes/config \
-        token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
-        kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" \
-        kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+vault secrets enable -path=kv kv-v2
 
-    vault policy write argocd - <<EOF
-path "kv/k3s/argocd/*" {
+vault auth enable kubernetes
+vault write auth/kubernetes/config \
+    token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+    kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" \
+    kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
+vault policy write argocd - <<EOF
+path "kv/data/k3s/argocd/*" {
   capabilities = ["read"]
 }
 EOF
 
-    vault write auth/kubernetes/role/argocd \
-        bound_service_account_names=argocd-server \
-        bound_service_account_namespaces=argocd \
-        policies=argocd \
-        ttl=24h
+vault write auth/kubernetes/role/argocd \
+    bound_service_account_names="*" \
+    bound_service_account_namespaces=argocd \
+    policies=argocd \
+    ttl=24h
 
-    vault auth enable approle
+#####
 
-    vault write auth/approle/role/argocd \
-        secret_id_ttl=10m \
-        token_num_uses=10 \
-        token_ttl=20m \
-        token_max_ttl=30m \
-        secret_id_num_uses=40
+vault auth enable approle
 
-    vault read auth/approle/role/argocd/role-id | grep role_id | tr -s ' ' | cut -d ' ' -f 2
+vault write auth/approle/role/argocd \
+    bound_service_account_names="*" \
+    bound_service_account_namespaces=argocd \
+    token_policies=argocd
 
-    # Get a SecretID issued against the argocd AppRole
-    vault write -f auth/approle/role/argocd/secret-id
+vault read auth/approle/role/argocd/role-id | grep role_id | tr -s ' ' | cut -d ' ' -f 2
 
-
-
+# Get a SecretID issued against the argocd AppRole
+vault write -f auth/approle/role/argocd/secret-id
