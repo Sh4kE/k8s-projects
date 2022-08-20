ee06730d015f52bc319bd3512173f672e950e4c7
Sh4kE <michael.wittig@posteo.de>
2022-08-20 13:42:45 +0200

    bootstrap

--- vault/bootstrap.sh
+++ vault/bootstrap.sh
@@ -1,12 +1,12 @@
-kubectl exec vault-0 -- vault operator init -key-shares=5 -key-threshold=3 -format=json > cluster-keys.json
+kubectl exec vault-0 -- vault operator init -key-shares=5 -key-threshold=3 -format=json > cluster-keys.wavestack.json
 
-UNSEAL_TOKEN_0=$(jq -r ".unseal_keys_b64[0]" cluster-keys.json)
-UNSEAL_TOKEN_1=$(jq -r ".unseal_keys_b64[1]" cluster-keys.json)
-UNSEAL_TOKEN_2=$(jq -r ".unseal_keys_b64[2]" cluster-keys.json)
-UNSEAL_TOKEN_3=$(jq -r ".unseal_keys_b64[3]" cluster-keys.json)
-UNSEAL_TOKEN_4=$(jq -r ".unseal_keys_b64[4]" cluster-keys.json)
+UNSEAL_TOKEN_0=$(jq -r ".unseal_keys_b64[0]" cluster-keys.wavestack.json)
+UNSEAL_TOKEN_1=$(jq -r ".unseal_keys_b64[1]" cluster-keys.wavestack.json)
+UNSEAL_TOKEN_2=$(jq -r ".unseal_keys_b64[2]" cluster-keys.wavestack.json)
+UNSEAL_TOKEN_3=$(jq -r ".unseal_keys_b64[3]" cluster-keys.wavestack.json)
+UNSEAL_TOKEN_4=$(jq -r ".unseal_keys_b64[4]" cluster-keys.wavestack.json)
 
-ROOT_TOKEN=$(jq -r ".root_token" cluster-keys.json)
+ROOT_TOKEN=$(jq -r ".root_token" cluster-keys.wavestack.json)
 
 kubectl exec vault-0 -- vault operator unseal $UNSEAL_TOKEN_0
 kubectl exec vault-0 -- vault operator unseal $UNSEAL_TOKEN_1
