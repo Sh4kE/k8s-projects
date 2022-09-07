212d2b7cacfd1777210a4211b82734da72d365e8
Sh4kE <michael.wittig@posteo.de>
2022-09-07 23:23:15 +0200

    remove obsolete stuffz

--- vault/bootstrap.sh
+++ vault/bootstrap.sh
@@ -21,29 +21,3 @@ kubectl exec -ti vault-2 -- vault operator raft join http://vault-0.vault-intern
 kubectl exec -ti vault-2 -- vault operator unseal $UNSEAL_TOKEN_0
 kubectl exec -ti vault-2 -- vault operator unseal $UNSEAL_TOKEN_1
 kubectl exec -ti vault-2 -- vault operator unseal $UNSEAL_TOKEN_2
-
-
-kubectl exec --stdin=true --tty=true vault-0 -- /bin/sh
-###############
-
-vault login -address=https://vault.k3s.sh4ke.rocks $ROOT_TOKEN
-
-vault secrets enable -path=kv kv-v2
-
-vault auth enable kubernetes
-vault write auth/kubernetes/config \
-    token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
-    kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" \
-    kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-
-vault policy write argocd - <<EOF
-path "kv/data/k3s/argocd/*" {
-  capabilities = ["read"]
-}
-EOF
-
-vault write auth/kubernetes/role/argocd \
-    bound_service_account_names="*" \
-    bound_service_account_namespaces=argocd \
-    policies=argocd \
-    ttl=24h
