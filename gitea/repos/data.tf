d9c10f5b461c6ec07a7bcd3a0e9593af7db9929a
Michael Wittig <michael.wittig@posteo.de>
2023-04-05 22:23:37 +0200

    remove non working tf gitea provider

--- gitea/repos/data.tf
+++
@@ -1,9 +0,0 @@
-data "gitea_repo" "repos" {
-  count    = length(local.repos)
-  name     = local.repos[count.index]
-  username = "sh4ke"
-}
-
-output "foo" {
-  value = data.gitea_repo.repos[12].created
-}
--- gitea/repos/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "gitea_url" {
-  type = string
-  default = "https://gitea.sh4ke.rocks"
-}
-
-variable "gitea_token" {
-  type = string
-  sensitive = true
-}
