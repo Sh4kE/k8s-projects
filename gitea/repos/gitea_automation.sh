3a36ebbc03534d71691ffe25f3e7d9c42edf180d
Michael Wittig <michael.wittig@posteo.de>
2023-04-06 11:12:31 +0200

    list all repos

--- gitea/repos/gitea_automation.sh
+++ gitea/repos/gitea_automation.sh
@@ -5,3 +5,5 @@ GITEA_TOKEN=$1
 user_id=$(curl -X "GET" "https://gitea.sh4ke.rocks/api/v1/user?access_token=$GITEA_TOKEN" -H "accept: application/json" | jq '.id')
 echo "user id of sh4ke is: $user_id"
 
+repos=$(curl -X 'GET' 'https://gitea.sh4ke.rocks/api/v1/repos/search?uid=2&access_token=***REMOVED***' -H 'accept: application/json' | jq -r '.data[] | .name')
+echo $repos
