3a6998b4354aea0d16cf875d15d99d736e5838b7
Michael Wittig <michael.wittig@posteo.de>
2023-04-06 11:14:30 +0200

    remove secret again -.-

--- gitea/repos/gitea_automation.sh
+++ gitea/repos/gitea_automation.sh
@@ -2,8 +2,8 @@
 
 GITEA_TOKEN=$1
 
-user_id=$(curl -X "GET" "https://gitea.sh4ke.rocks/api/v1/user?access_token=$GITEA_TOKEN" -H "accept: application/json" | jq '.id')
+user_id=$(curl -X "GET" "https://gitea.sh4ke.rocks/api/v1/user?access_token=$GITEA_TOKEN" -H "accept: application/json" | jq ".id")
 echo "user id of sh4ke is: $user_id"
 
-repos=$(curl -X 'GET' 'https://gitea.sh4ke.rocks/api/v1/repos/search?uid=2&access_token=***REMOVED***' -H 'accept: application/json' | jq -r '.data[] | .name')
-echo $repos
+repos=$(curl -X "GET" "https://gitea.sh4ke.rocks/api/v1/repos/search?uid=2&access_token=$GITEA_TOKEN" -H "accept: application/json" | jq -r ".data[] | .name")
+echo "$repos"
