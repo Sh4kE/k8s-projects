702b6c61dc41b000cc3c957d2832c9140c323f4c
Michael Wittig <michael.wittig@posteo.de>
2023-04-06 19:08:32 +0200

    add push mirrors

--- gitea/repos/gitea_automation.sh
+++ gitea/repos/gitea_automation.sh
@@ -5,15 +5,15 @@ GITEA_TOKEN=$1
 repos=$(curl -s -X "GET" "https://gitea.sh4ke.rocks/api/v1/user/repos?access_token=$GITEA_TOKEN" -H "accept: application/json" | jq -r ".[] | .name")
 for repo in $repos; do
     echo "$repo"
-    remotes=$(curl -s -X "GET" "https://gitea.sh4ke.rocks/api/v1/repos/sh4ke/$repo/push_mirrors?access_token=$GITEA_TOKEN" -H "accept: application/json" | jq -r ".[] | .remote_address")
-    found_gitea_ws=false
-    for remote in $remotes; do
-        echo "found remote: $remote"
-        if [[ "$remote" == *"https://gitea.ws.sh4ke.rocks/sh4ke/$repo.git"* ]]; then
-            found_gitea_ws=true
-        fi
-    done
-    if [ "$found_gitea_ws" = false ] ; then
-        echo "need to add remote for repo $repo"
-    fi
+    curl -X 'POST' \
+      "https://gitea.sh4ke.rocks/api/v1/repos/sh4ke/$repo/push_mirrors?access_token=$GITEA_TOKEN" \
+      -H "accept: application/json" \
+      -H "Content-Type: application/json" \
+      -d "{
+          \"interval\": \"8h0m0s\",
+          \"remote_address\": \"https://gitea.ws.sh4ke.rocks/sh4ke/$repo.git\",
+          \"remote_password\": \"$GITEA_TOKEN\",
+          \"remote_username\": \"sh4ke\",
+          \"sync_on_commit\": true
+        }"
 done
