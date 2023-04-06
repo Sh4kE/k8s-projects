c6eda7b50ea2bdaf94c5501eda32820372b7f395
Michael Wittig <michael.wittig@posteo.de>
2023-04-06 11:23:39 +0200

    search remotes

--- gitea/repos/gitea_automation.sh
+++ gitea/repos/gitea_automation.sh
@@ -6,4 +6,11 @@ user_id=$(curl -X "GET" "https://gitea.sh4ke.rocks/api/v1/user?access_token=$GIT
 echo "user id of sh4ke is: $user_id"
 
 repos=$(curl -X "GET" "https://gitea.sh4ke.rocks/api/v1/repos/search?uid=2&access_token=$GITEA_TOKEN" -H "accept: application/json" | jq -r ".data[] | .name")
-echo "$repos"
+
+for repo in $repos; do
+    echo "$repo"
+    remotes=$(curl -X "GET" "https://gitea.sh4ke.rocks/api/v1/repos/sh4ke/$repo/push_mirrors?access_token=$GITEA_TOKEN" -H "accept: application/json" | jq ".[] | .remote_address")
+    for remote in $remotes; do
+        echo "found remote: $remote"
+    done
+done
